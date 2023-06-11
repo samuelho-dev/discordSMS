683780a49fadde5e7899e89b5d2f9b575bedd9fc
samuelho-dev <samuelho343@gmail.com>
2023-06-10 21:14:29 -0700

    cleanup

--- src/discord/bot.ts
+++ src/discord/bot.ts
@@ -1,6 +1,6 @@
 import { Client } from 'discord.js';
 import config from '../config';
-import * as commandModules from '~/commands';
+import * as commandModules from '../commands';
 
 export const client = new Client({
   intents: ['Guilds', 'GuildMessages', 'DirectMessages'],
@@ -12,10 +12,6 @@ client.once('ready', () => {
   console.log('🤖 Bot is ready.');
 });
 
-const accountSid = config.TWILIO_ACCOUNT_SID;
-const authToken = config.TWILIO_AUTH_TOKEN;
-const twilioClient = require('twilio')(accountSid, authToken);
-
 client.on('interactionCreate', async (interaction) => {
   if (!interaction.isCommand()) {
     return;
@@ -23,13 +19,6 @@ client.on('interactionCreate', async (interaction) => {
 
   const { commandName } = interaction;
   commands[commandName].execute(interaction, client);
-  // twilioClient.messages
-  //   .create({
-  //     body: smsMessage,
-  //     from: config.TWILIO_PHONE_NUMBER,
-  //     to: subscriber.phoneNumber,
-  //   })
-  // }
 });
 
 client.login(config.DISCORD_TOKEN);
