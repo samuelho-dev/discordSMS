310d6ae313b9e6d042b8a661f39f0b0551e4e5e3
samuelho-dev <samuelho343@gmail.com>
2023-06-10 21:13:56 -0700

    abstract command deployment

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,17 +1,14 @@
-import { REST, Routes, SlashCommandBuilder } from 'discord.js';
+import * as commandModules from './commands';
+import { REST, Routes } from 'discord.js';
 import config from './config';
 const { DISCORD_TOKEN, DISCORD_GUILD_ID, DISCORD_CLIENT_ID } = config;
-import * as commandModules from '~/commands';
 
-const commands = [
-  new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('Replies with pong!'),
-  new SlashCommandBuilder()
-    .setName('sendSMS')
-    .setDescription('Send an SMS to a group.'),
-];
+type Command = { data: unknown };
+const commands = [];
 
+for (const module of Object.values<Command>(commandModules)) {
+  commands.push(module.data);
+}
 const rest = new REST({ version: '10' }).setToken(DISCORD_TOKEN!);
 
 rest
