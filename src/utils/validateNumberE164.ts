8a0bdb920e835a4402701c00e9ca2d1a4d19224f
samuelho-dev <samuelho343@gmail.com>
2023-06-11 16:17:55 -0700

    export

--- src/utils/validateNumberE164.ts
+++ src/utils/validateNumberE164.ts
@@ -1,4 +1,4 @@
-function validatePhoneForE164(phoneNumber: string) {
+export default function validatePhoneForE164(phoneNumber: string) {
   const regEx = /^\+[1-9]\d{10,14}$/;
 
   return regEx.test(phoneNumber);
