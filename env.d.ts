9a3b506add680d7ebb68165433a78f837ba77d3c
samuelho-dev <samuelho343@gmail.com>
2023-06-10 20:04:30 -0700

    common js and env

--- env.d.ts
+++ env.d.ts
@@ -1,15 +1,11 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    DISCORD_APPLICATION_ID: string;
     TWILIO_ACCOUNT_SID: string;
     TWILIO_AUTH_TOKEN: string;
     TWILIO_PHONE_NUMBER: string;
-    PUBLIC_KEY: string;
-    PORT: string;
     DATABASE_URL: string;
-    PLANETSCALE_USERNAME: string;
-    PLANETSCALE_PASSWORD: string;
-    DISCORD_CLIENT_SECRET: string;
+    DISCORD_TOKEN: string;
     DISCORD_CLIENT_ID: string;
+    DISCORD_GUILD_ID: string;
   }
 }
