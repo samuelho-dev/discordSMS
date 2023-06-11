13f1c4c935ae4f9de2e931a838f168556f60e952
samuelho-dev <samuelho343@gmail.com>
2023-06-11 01:50:43 -0700

    comment init

--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,65 @@
-require('dotenv').config();
-import express from 'express';
+import './discord/bot';
 
-import { GatewayIntentBits, REST, Routes, WebSocketManager } from 'discord.js';
-import db from './knex';
+// import express from 'express';
+// import db from './knex';
+// import config from './config';
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+// const app = express();
+// const PORT = 3000;
 
-// TWILIO
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const twilioClient = require('twilio')(accountSid, authToken);
+// app.post('/interactions', async function (req, res) {
+//   const { type, id, data } = req.body;
 
-const rest = new REST({ version: '10' }).setToken(
-  process.env.DISCORD_APPLICATION_ID,
-);
+//   const { name, options } = data;
 
-app.post('/interactions', async function (req, res) {
-  const { type, id, data } = req.body;
+//   if (name === 'createsms' && id) {
+//     const smsMessage = options[0].value;
 
-  const { name, options } = data;
+//     // Fetch all phone numbers from the database
+//     let phoneNumbers = await db('subscribers').select('phoneNumber');
 
-  if (name === 'createsms' && id) {
-    const smsMessage = options[0].value;
+//     // Send an SMS to each phone number
+//     for (let subscriber of phoneNumbers) {
+//       twilioClient.messages
+//         .create({
+//           body: smsMessage,
+//           from: config.TWILIO_PHONE_NUMBER,
+//           to: subscriber.phoneNumber,
+//         })
+//         .then((message) =>
+//           console.log(`Message sent to ${subscriber.phoneNumber}`),
+//         )
+//         .catch((err) =>
+//           console.error(
+//             `Failed to send SMS to ${subscriber.phoneNumber}: ${err.message}`,
+//           ),
+//         );
+//     }
+//     return res.send({
+//       type: '',
+//       data: {
+//         content: 'Done',
+//       },
+//     });
+//   }
+// });
 
-    // Fetch all phone numbers from the database
-    let phoneNumbers = await db('subscribers').select('phoneNumber');
+// app.post('subscribe', async function (req, res) {
+//   const incomingNumber = req.body.From;
+//   const smsBody = req.body.Body;
 
-    // Send an SMS to each phone number
-    for (let subscriber of phoneNumbers) {
-      twilioClient.messages
-        .create({
-          body: smsMessage,
-          from: process.env.TWILIO_PHONE_NUMBER,
-          to: subscriber.phoneNumber,
-        })
-        .then((message) =>
-          console.log(`Message sent to ${subscriber.phoneNumber}`),
-        )
-        .catch((err) =>
-          console.error(
-            `Failed to send SMS to ${subscriber.phoneNumber}: ${err.message}`,
-          ),
-        );
-    }
-    return res.send({
-      type: '',
-      data: {
-        content: 'Done',
-      },
-    });
-  }
-});
+//   if (smsBody === 'SUBSCRIBE') {
+//   }
+// });
 
-app.post('subscribe', async function (req, res) {
-  const incomingNumber = req.body.From;
-  const smsBody = req.body.Body;
+// app.post('unsubscribe', async function (req, res) {
+//   const incomingNumber = req.body.From;
+//   const smsBody = req.body.Body;
 
-  if (smsBody === 'SUBSCRIBE') {
-  }
-});
+//   if (smsBody === 'STOP') {
+//   }
+// });
 
-app.post('unsubscribe', async function (req, res) {
-  const incomingNumber = req.body.From;
-  const smsBody = req.body.Body;
-
-  if (smsBody === 'STOP') {
-  }
-});
-
-app.listen(PORT, () => {
-  console.log('Listening on port', PORT);
-});
+// app.listen(PORT, () => {
+//   console.log('Listening on port', PORT);
+// });
