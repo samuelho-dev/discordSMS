5a9c279ef200cf709e6600f8bfb78236e1a26602
samuelho-dev <samuelho343@gmail.com>
2023-06-11 10:21:55 -0700

    date schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,18 +1,16 @@
-import db from './knex';
-
 declare module 'knex/types/tables' {
   interface Member {
     id: number;
     guild_id: string;
     phone_number: string;
-    created_at: string;
+    created_at: Date;
     active: boolean;
-    updated_at: string;
+    updated_at: Date;
   }
   interface Message {
     id: number;
     guild_id: string;
     message: string;
-    sent_at: string;
+    sent_at: Date;
   }
 }
