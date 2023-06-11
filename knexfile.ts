b62a39b3f2529ee235548502e12a07b046e7e100
samuelho-dev <samuelho343@gmail.com>
2023-06-11 01:48:56 -0700

    added knex file migration

---
+++ knexfile.ts
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+import type { Knex } from 'knex';
+
+// Update with your config settings.
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+  },
+
+  staging: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+};
+
+module.exports = config;
