b31b61d2ce59a072a391c5d0366136d6f36cd8cd
samuelho-dev <samuelho343@gmail.com>
2023-06-11 00:40:51 -0700

    add error handling and comments

--- src/commands/sendsms.ts
+++ src/commands/sendsms.ts
@@ -4,14 +4,14 @@ import {
   GuildMember,
   SlashCommandBuilder,
 } from 'discord.js';
-import { Member } from 'knex/types/tables';
+import { Member, Message } from 'knex/types/tables';
 import db from '../knex';
 import twilioClient from '../twilio';
 import config from '../config';
 
 export const data = new SlashCommandBuilder()
   .setName('sendsms')
-  .setDescription('Send a SMS to a group.')
+  .setDescription('Send an SMS to your group.')
   .addStringOption((option) =>
     option
       .setName('message')
@@ -23,26 +23,67 @@ export const data = new SlashCommandBuilder()
 export async function execute(interaction: CommandInteraction, client: Client) {
   const member = interaction.member as GuildMember;
 
+  // IF THE MESSAGE ISNT A STRING
+  const message = interaction.options.get('message')?.value;
+  if (typeof message !== 'string') return;
+
+  // MEMBER MUST BE AN ADMIN
   if (!member.permissions.has('Administrator')) {
     return interaction.reply(
       'You do not have the required permissions to use this command.',
     );
   }
+
   const members = db<Member>('members');
+  const messages = db<Message>('messages');
+
+  // GET ALL PHONE NUMBERS
   const phoneNumbers = await members
     .where('guild_id', interaction.guild?.id)
     .where('active', true)
     .select('phone_number');
 
+  const failedNumbers = [];
+
+  // SEND MESSAGE TO NUMBERS - PUSH FAILED NUMBERS TO ARR
   for (const number of phoneNumbers) {
     const validatedNumber = validatePhoneForE164(`+1${number}`);
-
     if (validatedNumber) {
-      twilioClient.messages.create({
-        from: config.TWILIO_PHONE_NUMBER,
-        to: `+1${number}`,
-      });
+      await twilioClient.messages
+        .create({
+          from: config.TWILIO_PHONE_NUMBER,
+          to: `+1${number}`,
+          body: message,
+        })
+        .catch((err) => {
+          console.error(err);
+          failedNumbers.push(number);
+        });
+    } else {
+      failedNumbers.push(number);
     }
   }
-  return void interaction.reply('Pong!');
+
+  // ADD THE MESSAGE TO DB
+  try {
+    await messages.insert({
+      guild_id: interaction.guild?.id,
+      message: message,
+    });
+  } catch (err) {
+    console.error(err);
+    return void interaction.reply(
+      `An error occured during message upload. ${
+        failedNumbers.length !== 0 &&
+        `Numbers that failed to send: ${failedNumbers.join('\n')}`
+      }`,
+    );
+  }
+
+  return void interaction.reply(
+    `Messages have been sent. ${
+      failedNumbers.length !== 0 &&
+      `Numbers that failed to send: ${failedNumbers.join('\n')}`
+    }`,
+  );
 }
