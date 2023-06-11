7f53ed39d59f0f40f12bc96439b0db9589f7363e
samuelho-dev <samuelho343@gmail.com>
2023-06-11 16:17:46 -0700

    migration init

--- migrations/20230611080030_init.js
+++ migrations/20230611080030_init.js
@@ -4,17 +4,25 @@
  */
 exports.up = function (knex) {
   return knex.schema
+    .createTable('guilds', function (table) {
+      table.increments('id');
+      table.string('guild_id').unique();
+      table.dateTime('created_at').defaultTo(knex.fn.now());
+    })
     .createTable('members', function (table) {
       table.increments('id');
       table.string('guild_id');
+      table.foreign('guild_id').references('guild_id').inTable('guilds');
       table.string('phone_number');
       table.boolean('active').defaultTo(true);
       table.dateTime('created_at').defaultTo(knex.fn.now());
       table.dateTime('updated_at').defaultTo(knex.fn.now());
+      table.unique(['guild_id', 'phone_number']);
     })
     .createTable('messages', function (table) {
       table.increments('id');
       table.string('guild_id');
+      table.foreign('guild_id').references('guild_id').inTable('guilds');
       table.string('message');
       table.dateTime('sent_at').defaultTo(knex.fn.now());
     });
@@ -25,5 +33,8 @@ exports.up = function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-  return knex.schema.dropTable('members').dropTable('messages');
+  return knex.schema
+    .dropTable('members')
+    .dropTable('messages')
+    .dropTable('guilds');
 };
