e6d9d988c8b2bfd2db654d0de43bb87e2979c6ce
samuelho-dev <samuelho343@gmail.com>
2023-06-11 18:17:29 -0700

    delete build

--- build/src/utils/validateNumberE164.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function validatePhoneForE164(phoneNumber) {
-    const regEx = /^\+[1-9]\d{10,14}$/;
-    return regEx.test(phoneNumber);
-}
-exports.default = validatePhoneForE164;
