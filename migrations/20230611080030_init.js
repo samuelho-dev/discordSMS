9e313389b912ed93cb83d2c08e0617938627cea4
samuelho-dev <samuelho343@gmail.com>
2023-06-17 23:11:35 -0700

    revise guilds

--- migrations/20230611080030_init.js
+++ migrations/20230611080030_init.js
@@ -7,6 +7,11 @@ exports.up = function (knex) {
     .createTable('guilds', (table) => {
       table.increments('id');
       table.string('guild_id').unique();
+      table.string('guild_name');
+      table.string('phone_number').unique().nullable();
+      table.string('sms_tag');
+      table.string('tagline');
+      table.dateTime('updated_at').defaultTo(knex.fn.now());
       table.dateTime('created_at').defaultTo(knex.fn.now());
     })
     .createTable('members', (table) => {
@@ -14,7 +19,6 @@ exports.up = function (knex) {
       table.string('guild_id');
       table.foreign('guild_id').references('guild_id').inTable('guilds');
       table.string('phone_number');
-      table.boolean('active').defaultTo(true);
       table.dateTime('created_at').defaultTo(knex.fn.now());
       table.dateTime('updated_at').defaultTo(knex.fn.now());
       table.unique(['guild_id', 'phone_number']);
