36ea0e0b785fd3d9a31c27d014107d70a99480f2
samuelho-dev <samuelho343@gmail.com>
2023-06-17 23:13:36 -0700

    update phone number cmd

---
+++ src/commands/updatePhoneNumber.ts
@@ -0,0 +1,63 @@
+import {
+  Client,
+  CommandInteraction,
+  GuildMember,
+  SlashCommandBuilder,
+} from 'discord.js';
+import { Guild } from 'knex/types/tables';
+import db from '../db/knex';
+
+export const data = new SlashCommandBuilder()
+  .setName('updatePhoneNumber')
+  .setDescription('Send an SMS to your group.')
+  .addStringOption((option) =>
+    option
+      .setName('TWILIO_PHONE_NUMBER')
+      .setDescription('Enter your TWILIO_PHONE_NUMBER.')
+      .setRequired(true)
+      .setMaxLength(10),
+  );
+
+export async function execute(interaction: CommandInteraction, client: Client) {
+  if (!interaction.guild || !interaction.guild.id) return;
+
+  // MEMBER MUST BE AN ADMIN
+  const member = interaction.member as GuildMember;
+  if (!member.permissions.has('Administrator')) {
+    return interaction.reply(
+      'You do not have the required permissions to use this command. ❌',
+    );
+  }
+
+  const TWILIO_PHONE_NUMBER = interaction.options.get('TWILIO_PHONE_NUMBER');
+  if (
+    !TWILIO_PHONE_NUMBER ||
+    !TWILIO_PHONE_NUMBER.value ||
+    typeof TWILIO_PHONE_NUMBER.value !== 'string'
+  ) {
+    return interaction.reply('Please input your TWILIO_PHONE_NUMBER.');
+  }
+
+  const guild = db<Guild>('guild');
+
+  try {
+    await guild
+      .where({
+        guild_id: interaction.guild.id,
+      })
+      .insert({
+        phone_number: TWILIO_PHONE_NUMBER.value,
+      })
+      .onConflict(['TWILIO_PHONE_NUMBER'])
+      .merge({
+        phone_number: TWILIO_PHONE_NUMBER.value,
+      });
+  } catch (err) {
+    console.error(err);
+    return interaction.reply(
+      'An error occured while updating your phone number, please check your credentials.',
+    );
+  }
+
+  return void interaction.reply(`Messages have been sent.`);
+}
