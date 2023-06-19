2a64d3e7d4600cff31586b90ce837fd374b8baa6
samuelho-dev <samuelho343@gmail.com>
2023-06-18 21:30:37 -0700

    add modules

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,3 +1,5 @@
+export * as register from './register';
 export * as sendsms from './sendsms';
 export * as subscribe from './subscribe';
 export * as unsubscribe from './unsubscribe';
+export * as updatePhoneNumber from './updatePhoneNumber';
